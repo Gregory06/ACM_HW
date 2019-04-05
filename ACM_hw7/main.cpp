474dbc6d488fed298fbc28a8e87f04ab2f09cc0c
Gregory Postnikov <greg@MacBook-Pro-Gregory.local>
2019-04-06 00:17:26 +0300

    1.2

--- ACM_hw7/main.cpp
+++ ACM_hw7/main.cpp
@@ -40,91 +40,6 @@ namespace FFT {
     std::vector<T> FastInverseFourierTransform(const std::vector<T>& data);
 } // namespace FFT
 
-
-// Операции над многочленами с помощью ффт
-template <typename T>
-class Polynomial {
-public:
-    explicit Polynomial(const std::vector<T>& coefficients)
-        : coefficients_(coefficients), degree_(coefficients.size()) {
-    }
-
-    // Чтобы можно было написать Polynomial<std::complex<double>> p({1, 2, 1})
-    // И получить представление многочлена 1 + 2x + x^2
-    Polynomial(const std::initializer_list<T>& coefficients)
-        : coefficients_(coefficients.begin(), coefficients.end()), degree_(coefficients.size()) {
-    }
-
-    Polynomial(const Polynomial&) = default;
-    Polynomial(Polynomial&&) noexcept = default;
-
-//    Polynomial& operator=(const Polynomial) = default;
-//    Polynomial& operator=(Polynomial&&) noexcept = default;
-
-    size_t GetDegree() const {
-        return degree_;
-    }
-
-    // Реализуйте следующие методы
-    bool operator==(const Polynomial& other);
-
-    Polynomial& clean();
-
-    Polynomial& operator+=(const Polynomial& other);
-
-    Polynomial& operator-=(const Polynomial& other);
-
-    // Возведение в степень pow с помощью комбинации FFT и индийского возведения в степень
-    Polynomial& operator^=(size_t pow);
-
-    // С Использованием быстрого преобразования фурье
-    Polynomial& operator*=(const Polynomial& other);
-
-    friend std::ostream& operator<<(std::ostream& ostr, const Polynomial& a);
-
-    // Используя предыдущее
-    Polynomial operator+(const Polynomial& other);
-
-    Polynomial operator-(const Polynomial& other);
-
-    Polynomial operator*(const Polynomial& other);
-
-    Polynomial operator^(size_t pow);
-
-    // И еще один, унарный минус
-    friend Polynomial operator-(const Polynomial& other);
-
-private:
-    std::vector<T> coefficients_;
-    size_t degree_;
-};
-
-
-// Напишите какие-то тесты, демонстрирующие корректность написанных вами методов
-
-
-
-// Задачи, решаемые с помощью умножения многочленов
-// Если вы напишете решение, работающее для произольных строк над ascii кодировкой - укажете это и
-// возможно, получите небольшой бонусный балл
-namespace SubstringMatching {
-
-    // Метод принимает две строки str и pattern, возвращает индексов,
-    // указывающих начала подстрок str, совпадающих с pattern
-    // Можете считать, что str и pattern состоят из символов 0 и 1
-    std::vector<size_t> FindSubstrings(const std::string& str, const std::string& pattern);
-
-    // Аналогично предыдущему, но теперь pattern может содержать символ '?', на месте которого
-    // можно поставить любой символ алфавита
-    std::vector<size_t> FindMatches(const std::string& str, const std::string& pattern);
-
-} // namespace SubstringMatcher
-
-
-
-// Напишите какие-то тесты, демонстрирующие корректность написанных вами методов
-// Обратите внимание, что при проведении прямого и обратного преобразования для многочлена с
-// целыми коэффициентами, новые коэффициенты могут выйти дробными и трубующими округления.
 template <typename T>
 T FFT::GetRoot(size_t degree) {
     return T(cos(2*PI / degree), sin(2*PI / degree));
@@ -219,9 +134,73 @@ template <typename T>
 std::vector<T> FFT::AddPadding(const std::vector<T>& data, size_t expected_length) {
     std::vector<T> result(data);
     for (size_t i = data.size(); i < expected_length; i++)
-        result.push_back(T(0,0));
+        result.push_back(T(0));
     return result;
 }
+
+
+// Операции над многочленами с помощью ффт
+template <typename T>
+class Polynomial {
+public:
+    explicit Polynomial(const std::vector<T>& coefficients)
+        : coefficients_(coefficients), degree_(coefficients.size()) {
+    }
+
+    // Чтобы можно было написать Polynomial<std::complex<double>> p({1, 2, 1})
+    // И получить представление многочлена 1 + 2x + x^2
+    Polynomial(const std::initializer_list<T>& coefficients)
+        : coefficients_(coefficients.begin(), coefficients.end()), degree_(coefficients.size()) {
+    }
+
+    Polynomial(const Polynomial&) = default;
+    Polynomial(Polynomial&&) noexcept = default;
+
+//    Polynomial& operator=(const Polynomial) = default;
+//    Polynomial& operator=(Polynomial&&) noexcept = default;
+
+    std::vector<T>  GetCoefficients() const {
+        return coefficients_;
+    }
+
+    size_t GetDegree() const {
+        return degree_;
+    }
+
+    // Реализуйте следующие методы
+    bool operator==(const Polynomial& other);
+
+    Polynomial& clean();
+
+    Polynomial& operator+=(const Polynomial& other);
+
+    Polynomial& operator-=(const Polynomial& other);
+
+    // Возведение в степень pow с помощью комбинации FFT и индийского возведения в степень
+    Polynomial& operator^=(size_t pow);
+
+    // С Использованием быстрого преобразования фурье
+    Polynomial& operator*=(const Polynomial& other);
+
+    friend std::ostream& operator<<(std::ostream& ostr, const Polynomial& a);
+
+    // Используя предыдущее
+    Polynomial operator+(const Polynomial& other);
+
+    Polynomial operator-(const Polynomial& other);
+
+    Polynomial operator*(const Polynomial& other);
+
+    Polynomial operator^(size_t pow);
+
+    // И еще один, унарный минус
+    friend Polynomial operator-(const Polynomial& other);
+
+private:
+    std::vector<T> coefficients_;
+    size_t degree_;
+};
+
 
 template <typename T>
 Polynomial<T>& Polynomial<T>::clean() {
@@ -234,7 +213,7 @@ Polynomial<T>& Polynomial<T>::clean() {
         if (coefficients_[i].real() < eps)
             coefficients_[i] = T(0, coefficients_[i].imag());
         if (coefficients_[i].imag() < eps)
-            coefficients_[i] = T(coefficients_[i].real(), 0);
+            coefficients_[i] = T(coefficients_[i].real());
 
     }
     degree_ = coefficients_.size();
@@ -249,13 +228,6 @@ Polynomial<T>& Polynomial<T>::operator*=(const Polynomial<T>& other) {
     std::vector<T> added_other = FFT::AddPadding(other.coefficients_, expected_length);
     std::vector<T> fft_this = FFT::FourierTransform(added_this);
     std::vector<T> fft_other = FFT::FourierTransform(added_other);
-
-//    for (int i = 0; i < fft_this.size(); i++)
-//        std::cout << fft_this[i] << ' ';
-//    std::cout << std::endl;
-//    for (int i = 0; i < fft_other.size(); i++)
-//        std::cout << fft_other[i] << ' ';
-//    std::cout << std::endl;
 
     for (int i = 0; i < expected_length; i++)
         fft_this[i] *= fft_other[i];
@@ -367,23 +339,95 @@ std::ostream& operator<<(std::ostream& ostr, const Polynomial<std::complex<doubl
     return ostr;
 }
 
+
+// Напишите какие-то тесты, демонстрирующие корректность написанных вами методов
+
+
+
+// Задачи, решаемые с помощью умножения многочленов
+// Если вы напишете решение, работающее для произольных строк над ascii кодировкой - укажете это и
+// возможно, получите небольшой бонусный балл
+namespace SubstringMatching {
+
+    // Метод принимает две строки str и pattern, возвращает индексов,
+    // указывающих начала подстрок str, совпадающих с pattern
+    // Можете считать, что str и pattern состоят из символов 0 и 1
+    std::vector<size_t> FindSubstrings(const std::string& str, const std::string& pattern);
+
+    // Аналогично предыдущему, но теперь pattern может содержать символ '?', на месте которого
+    // можно поставить любой символ алфавита
+    std::vector<size_t> FindMatches(const std::string& str, const std::string& pattern);
+
+} // namespace SubstringMatcher
+
+
+std::vector<size_t> SubstringMatching::FindSubstrings(const std::string& str, const std::string& pattern) {
+    std::vector<std::complex<double>> pattern_vec(pattern.begin(), pattern.end());
+    std::vector<std::complex<double>> corpus_vec(str.begin(), str.end());
+    size_t pattern_len(pattern.length()), corpus_len(str.length());
+    std::complex<double> pattern_sum(0);
+    std::complex<double> corpus_sum(0);
+    std::vector<size_t> result;
+
+    for (auto i = pattern_vec.begin(); i != pattern_vec.end(); i++)
+        pattern_sum += pow(*i, 2);
+    for (auto i = corpus_vec.begin(); i != corpus_vec.begin() + pattern_len; i++)
+        corpus_sum += pow(*i, 2);
+
+    std::reverse(pattern_vec.begin(), pattern_vec.end());
+    Polynomial pattern_poly(pattern_vec), corpus_poly(corpus_vec);
+    Polynomial multy_poly = pattern_poly * corpus_poly;
+    std::vector<std::complex<double>> multy_coefficents = multy_poly.GetCoefficients();
+
+    std::vector<std::complex<double>> output({pattern_sum - std::complex<double>(2)*multy_coefficents[pattern_len-1] + corpus_sum});
+    for(size_t i = 1; i < corpus_len - pattern_len; i++)
+        output.push_back(output[i-1] + std::complex<double>(2)*(multy_coefficents[pattern_len-2+i] - multy_coefficents[pattern_len-1+i]) - pow(corpus_vec[i-1],2) + pow(corpus_vec[pattern_len-1+i],2));
+
+    std::vector<std::complex<double>>::iterator itr = output.begin();
+    while (itr != output.end()) {
+        itr = std::find_if(itr, output.end(), [](std::complex<double> a) { return (abs(a) < eps); });
+        if (itr != output.end()) {
+            result.push_back(std::distance(output.begin(), itr));
+            itr++;
+        }
+    }
+    return result;
+}
+
+
+// Напишите какие-то тесты, демонстрирующие корректность написанных вами методов
+// Обратите внимание, что при проведении прямого и обратного преобразования для многочлена с
+// целыми коэффициентами, новые коэффициенты могут выйти дробными и трубующими округления.
+
+
+
 int main() {
+
+    std::string corpus("greg is the best the fuck"), pattern("the");
+
+    auto out = SubstringMatching::FindSubstrings(corpus, pattern);
+    for (auto i = out.begin(); i != out.end(); i++)
+        std::cout << *i << ' ';
+
+
+//    for (std::vector<std::complex<double>>::const_iterator i = v.begin(); i != v.end(); i++)
+//        std::cout << *i << ' ';
 
 //    std::cout << pow(FFT::GetRoot<std::complex<double>>(4), -2);
 //    Polynomial<std::complex<double>> p(std::vector<std::complex<double>>{1, 2, 1});
-    std::vector<std::complex<double>> a = std::vector<std::complex<double>>{1,2,1,0,5,6,7,8}, b = std::vector<std::complex<double>>{4,8,4,12};
+//    std::vector<std::complex<double>> a = std::vector<std::complex<double>>{1,2,1,0,5,6,7,8}, b = std::vector<std::complex<double>>{4,8,4,12};
 //    std::vector<std::complex<double>> a = FFT::AddPadding<std::complex<double>>(std::vector<std::complex<double>>{1,1}, pow(2, (size_t) std::ceil(log(4)/log(2))));
 //    for (std::vector<std::complex<double>>::const_iterator i = a.begin(); i != a.end(); ++i)
 //        std::cout << *i << ' ';
 //    std::vector<std::complex<double>> b = FFT::FastFourierTransform(a);
-    std::vector<std::complex<double>> c = FFT::FastInverseFourierTransform(a);
+//    std::vector<std::complex<double>> c = FFT::FastInverseFourierTransform(a);
 
 //    std::vector<std::complex<double>> c = FFT::FastInverseFourierTransform(std::vector<std::complex<double>>{1,1});
-    Polynomial<std::complex<double>> p1({1, 2}), p2({1, 1});
+//    Polynomial<std::complex<double>> p1({1, 2}), p2({1, 1});
 
-    Polynomial<std::complex<double>> p3 = p1 ^ 3;
+//    Polynomial<std::complex<double>> p3 = p1 ^ 3;
 
-    std::cout << p3;
+//    std::cout << p3;
 //    std::cout << std::endl;
 //    for (std::vector<std::complex<double>>::const_iterator i = c.begin(); i != c.end(); ++i)
 //        std::cout << *i << ' ';
